2b30ff808e728a39c65ef5cd957e9994f400f34d
hhenriqu <heliohenriquemoraes@gmail.com>
2022-09-01 12:23:55 -0300

    init

---
+++ CapBankNG/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        // Front a ser desenvolvido posteriormente em ANGULAR //
+        System.out.println("Bem Vindo ao CapBank!");
+        System.out.println("Um banco fictício com conhecimento real.");
+
+
+
+
+    }
+}
